9f851eab501eb329e0b3dadb424a41a789b5bf6b
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2023-03-08 19:07:20 -0300

    Init socket connection handler

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socket = SocketIO(app)
+
+@socket.on('connect')
+def handle_connect():
+    print('Novo cliente conectado')
 
 @app.route("/")
 def hello_world():
