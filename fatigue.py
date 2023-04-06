767a2e15cc0d651b8822eedea5e2c5df54f67715
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2023-04-06 20:40:29 -0300

    Fatigue classes and integration with hypnos api

---
+++ fatigue.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+class KssScaleEnum(Enum):
+    EXTREMELY_ALERT = 1
+    VERY_ALERT = 2
+    ALERT = 3
+    FAIRLY_ALERT = 4
+    NEITHER_ALERT_NOR_SLEEPY = 5
+    SOME_SIGNS_OF_SLEEPINESS = 6
+    SLEEPY_BUT_NOT_EFFORT_TO_KEEP_ALERT = 7
+    SLEEPY_SOME_EFFORT_TO_KEEP_ALERT = 8
+    VERY_SLEEPY = 9
+
+
+class FatigueDetectionInfo:
+    def __init__(self, mouth: dict, eyes: dict, head: dict):
+        self.mouth = mouth
+        self.eyes = eyes
+        self.head = head
+
+    def get_dictionary(self):
+        dictionary = {
+            'mouth': self.mouth,
+            'eyes': self.eyes,
+            'head': self.eyes
+        }
+        return dictionary
+
+
+class FatigueStatus:
+    def __init__(self, kssScale: KssScaleEnum, detection: FatigueDetectionInfo):
+        self.kssScale = kssScale
+        self.detection = detection
+
+    def get_dictionary(self):
+        dictionary = {
+            'kss': self.kssScale,
+            'detection': self.detection.get_dictionary()
+        }
+        return dictionary
+
+detection = FatigueDetectionInfo(mouth={'teste': 'teste'},
+                                 eyes={'teste': 'teste'},
+                                 head={'teste': 'teste'})
+example_status = FatigueStatus(kssScale=1, detection=detection)
--- image_receiver.py
+++ image_receiver.py
@@ -1,8 +1,8 @@
 import asyncio
 import socketio
 from time import sleep
-import random
 from base64_functions import base64_2_cvimage
+from fatigue import FatigueStatus, example_status
 
 
 class SocketDataRequest:
@@ -11,37 +11,32 @@ class SocketDataRequest:
         self.employee_id = employee_id
         self.workstation = workstation
         self.images = images
-
-
-
-class ImageStatus:
-    def __init__(self, kss: int, detection: dict[str, str | int]):
-        self.kss = kss
-        self.detection = detection
-
-    def get_dictionary(self):
+    
+    def get_dictionary(self) -> dict:
         dictionary = {
-            "kss": self.kss,
-            "detection": self.detection
+            'id': self.id,
+            'employeeId': self.employee_id,
+            'workstation': self.workstation,
+            'images': self.images
         }
         return dictionary
 
 
 class SocketDataResponse:
-    def __init__(self, id: str, image_status: ImageStatus):
+    def __init__(self, id: str, employee_id: str, workstation: str, image_status: FatigueStatus):
         self.id = id
+        self.employee_id = employee_id
+        self.workstation = workstation
         self.image_status = image_status
 
     def get_dictionary(self):
         dictionary = { 
-            "id": self.id,
-            "imageStatus": self.image_status.get_dictionary()
+            'id': self.id,
+            'employeeId': self.employee_id,
+            'workstation': self.workstation,
+            'imageStatus': self.image_status.get_dictionary(),
         }
         return dictionary
-
-
-# dictionairy with the possible status returned to hypnos api after analysis
-status = {"1": 'Neutro', "2": "Há fadiga", "3": "Não há fadiga", "4": "Usuário não detectado"}
 
 sio = socketio.AsyncClient()
 
@@ -60,26 +55,21 @@ def connect_error(data):
 
 # this function is used to receive images from hypnos api and emit a response based on
 # the worker facial status after analysis
-@sio.on('server_to_drowsy')
+@sio.on('process-image')
 async def receive_images(data):
-    
-    # getting the base64 url
-    frame = list(data.values())[1][0]
-    rgb_image = base64_2_cvimage(frame)
+    request = SocketDataRequest(id=data['id'], employee_id=data['employeeId'], workstation=data['workstation'], images=data['images'])
+    rgb_image = base64_2_cvimage(request.images[0])
 
-    # printing images as base64 strings
-    print(rgb_image)
-    # this algorithm gets one frame and tests if the image was really received
-    #with open("imagensbase64.txt", "w") as base64:
-       #  base64.write(str(frame))
     '''
-    Enviar imagens para análise
-    '''
-    '''
-    Enviar imagens para análise
+    status = DrowsinessDetection().detectDrowsiness([rgb_image], fps)
     '''
 
-    await sio.emit('notify-status', random.choice(list(status.values())))
+    response = SocketDataResponse(id=request.id, 
+                                  employee_id=request.employee_id, 
+                                  workstation=request.workstation, 
+                                  image_status=example_status).get_dictionary()
+
+    await sio.emit('notify-status', response)
 
 
 async def connect_to_server():
