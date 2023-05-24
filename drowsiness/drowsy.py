e01562a27e77832c69c54b017d5ad2fed6a93285
FMABr <felipemuamorim@gmail.com>
2023-05-24 15:19:19 -0400

    Frame rate fix

--- drowsiness/drowsy.py
+++ drowsiness/drowsy.py
@@ -12,7 +12,7 @@ from drowsiness.detection import detector, eye, head, mouth
 classifier = KSSClassifier(0, 0, 0)
 handlers = CropHandler(ResizeHandler)
 
-FRAME_RATE = 24
+FRAME_RATE = 10
 insight_face = detector.InsightDetector
 mediapipe = detector.MediapipeDetector
 
@@ -25,9 +25,9 @@ def detect(video: list[np.ndarray]) -> FatigueStatus:
 
     in_results = map(insight_face["landmarks"], faces)
 
-    eye_result = eye.execute(in_results)
-    mouth_result = mouth.execute(in_results)
-    head_result = head.execute(mp_results, video[0].shape)
+    eye_result = eye.execute(in_results, fps=FRAME_RATE)
+    mouth_result = mouth.execute(in_results, fps=FRAME_RATE)
+    head_result = head.execute(mp_results, video[0].shape, fps=FRAME_RATE)
 
     classifier.set_results(eye_result, head_result, mouth_result)
 
